95aac39aacec14b09caefeefc8cbc09d079ab0c8
Dhruv <85575534+dhruvd21@users.noreply.github.com>
2025-12-02 02:45:19 +0530

    Implement path sum algorithm for binary tree

---
+++ path-sum-2.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+import java.util.*;
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> arr = new ArrayList<>();
+        dfs(root, 0, res, arr, targetSum);
+        return res;
+    }
+    public void dfs(TreeNode root, int curSum, List<List<Integer>> res, List<Integer> arr, int targetSum){
+        if(root == null){
+            return;
+        }
+        curSum += root.val;
+        arr.add(root.val);
+        if(root.left == null && root.right == null && curSum == targetSum){
+            res.add(new ArrayList<>(arr));
+        }
+        dfs(root.left, curSum, res, arr, targetSum);
+        dfs(root.right, curSum, res, arr, targetSum);
+        arr.remove(arr.size() - 1);
+        return;
+    }
+}
